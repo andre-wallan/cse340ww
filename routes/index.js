a5633220b89b62d57b873234740dfdf758ecdb7e
andre-wallan <andressemanda@gmail.com>
2026-01-08 21:38:07 +0300

    fff

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('index', { title: 'CSE Motors' });
 });
+router.get('/', (req, res) => {
+  res.send('CSE Motors is rendering');
+});
 
 module.exports = router;
