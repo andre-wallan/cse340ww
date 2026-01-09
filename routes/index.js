93d7e6cf98f6ef6ca030b94c72fe6dba692c00e1
andre-wallan <andressemanda@gmail.com>
2026-01-09 17:13:56 +0300

    yess

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
-/*const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'CSE Motors' });
 });
-router.get('/', (req, res) => {
-  res.send('CSE Motors is rendering');
-});
 
-module.exports = router; */
-app.get('/', (req, res) => {
-  res.send('CSE Motors is running!');
-});
+module.exports = router;
+
