edb3f3e74a23f455b597a9ab21b9de4154838bdd
andre-wallan <andressemanda@gmail.com>
2026-01-21 13:54:32 +0300

    db

--- public/js/account-update.js
+++
@@ -1,5 +0,0 @@
-const form = document.querySelector("#accountUpdateForm")
-form.addEventListener("change", function () {
-    const updateBtn = document.querySelector("button")
-    updateBtn.removeAttribute("disabled")
-})
