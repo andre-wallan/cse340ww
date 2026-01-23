2b9d44510e4b15c74f4e22541a2918745fd5b0a3
andre-wallan <andressemanda@gmail.com>
2026-01-23 15:17:33 +0300

    ind

--- utilities/index.js
+++ utilities/index.js
@@ -1,172 +1,170 @@
-const invUtil = require("./inventory-validation");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const invModel = require("../models/inventory-model")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const Util = {}
 
-const Util = {};
-
-/* ************************
+/* **
  * Constructs the nav HTML unordered list
- ************************** */
-Util.getNav = async function () {
-  try {
-    let data = await invModel.getClassifications();
-    console.log(data);
-    let list = "<ul>";
-    list += '<li><a href="/" title="Home page">Home</a></li>';
+ * **/
+Util.getNav = async function (req, res, next) {
+    let data = await invModel.getClassifications()
+    console.log(data)
+    let list = "<ul id='dynamic-nav'>"
+    list += '<li><a href="/" title="Home page">Home</a></li>' 
     data.rows.forEach((row) => {
-      list += "<li>";
-      list +=
-        '<a href="/inv/type/' +
-        row.classification_id +
-        '" title="See our inventory of ' +
-        row.classification_name +
-        ' vehicles">' +
-        row.classification_name +
-        "</a>";
-      list += "</li>";
-    });
-    list += "</ul>";
-    return list;
-  } catch (error) {
-    console.error("Error building navigation:", error.message);
-    throw new Error("Could not retrieve classifications.");
-  }
-};
+        list += "<li>"
+        list +=
+            '<a href="/inv/type/' +
+            row.classification_id +
+            '" title="See our inventory of ' +
+            row.classification_name +
+            ' vehicles">' +
+            row.classification_name +
+            '</a>'
+        list += "</li>"
+    })
+    list += "</ul>"
+    return list
+}
 
-/* **************************************
- * Build the classification view HTML
- ************************************** */
-Util.buildClassificationGrid = async function (data) {
-  let grid = ""; // Initialize grid to avoid undefined reference
-  if (data.length > 0) {
-    grid = '<ul id="inv-display">';
-    data.forEach((vehicle) => {
-      grid += '<li>';
-      grid +=
-        '<a href="../../inv/detail/' +
-        vehicle.inv_id +
-        '" title="View ' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        ' details"><img src="' +
-        vehicle.inv_thumbnail +
-        '" alt="' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        ' on CSE Motors" /></a>';
-      grid += '<div class="namePrice">';
-      grid += "<hr />";
-      grid += "<h2>";
-      grid +=
-        '<a href="../../inv/detail/' +
-        vehicle.inv_id +
-        '" title="View ' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        ' details">' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        "</a>";
-      grid += "</h2>";
-      grid +=
-        "<span>$" +
-        new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
-        "</span>";
-      grid += "</div>";
-      grid += "</li>";
-    });
-    grid += "</ul>";
-  } else {
-    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
-  }
-  return grid;
-};
-
-/* **************************************
- * Build HTML for vehicle pages
- ************************************** */
-Util.buildVehiclePage = async function (data) {
-  let vehicleTemplate = "";
-
-  if (data) {
-    vehicleTemplate += '<section id="vehicle-details">';
-
-    vehicleTemplate += '<div class="vehicle-container">';
-
-    vehicleTemplate += '<div class="vehicle-image-container">';
-    vehicleTemplate += `<img src="${data.inv_image}" alt="${data.inv_make} ${data.inv_model}">`;
-    vehicleTemplate += "</div>";
-
-    vehicleTemplate += '<div class="vehicle-details-container">';
-    vehicleTemplate += `<h2>${data.inv_make} ${data.inv_model}</h2>`;
-    vehicleTemplate += "<p>";
-    vehicleTemplate += `<strong>Year:</strong> ${data.inv_year}<br>`;
-    vehicleTemplate += `<strong>Color:</strong> ${data.inv_color}<br>`;
-    vehicleTemplate += `<strong>Miles:</strong> ${new Intl.NumberFormat("en-US").format(data.inv_miles)} miles<br>`;
-    vehicleTemplate += "</p>";
-    vehicleTemplate += `<p>${data.inv_description}</p>`;
-    vehicleTemplate += `<p class="price">Price: $${new Intl.NumberFormat("en-US").format(data.inv_price)}</p>`;
-    vehicleTemplate += "</div>";
-    vehicleTemplate += "</div>";
-    vehicleTemplate += "</section>";
-  } else {
-    vehicleTemplate =
-      '<p class="notice">Sorry, the vehicle you are looking for could not be found.</p>';
+/* **
+* Build the classification view HTML
+* ** */
+Util.buildClassificationGrid = async function(data){
+    let grid
+    if(data.length > 0){
+      grid = '<ul id="inv-display">'
+      data.forEach(vehicle => { 
+        grid += '<li>'
+        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+        + ' details"><img src="' + vehicle.inv_thumbnail 
+        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+        +' on CSE Motors"></a>'
+        grid += '<div class="namePrice">'
+        grid += '<hr>'
+        grid += '<h2>'
+        grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+        grid += '</h2>'
+        grid += '<span>$' 
+        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+        grid += '</div>'
+        grid += '</li>'
+      })
+      grid += '</ul>'
+    } else { 
+      grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+    }
+    return grid
   }
 
-  return vehicleTemplate;
-};
+  Util.buildInventoryDetails = async function(info) {
+    let details
+    if(info.length > 0) {
+      details = '<div id="inv-details">'
+      info.forEach(vehicle => {
+        details += '<div>'
+        details += '<img src="' + vehicle.inv_image + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + '">'
+        details += '</div>'
+        details += '<div class="inv-details-text">'
+        details += '<h3><strong>' + vehicle.inv_make + ' ' + vehicle.inv_model + ' details</strong></h3>'
+        details += '<p><strong>Price:</strong> $' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
+        details += '<p><strong>Description:</strong> ' + vehicle.inv_description + '</p>'
+        details += '<p><strong>Color:</strong> ' + vehicle.inv_color + '</p>'
+        details += '<p><strong>Mileage:</strong> ' + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + ' miles</p>'
+        details += '</div>'
+      })
+      details += '</div>'
+    } else {
+      details += '<p class="notice">Sorry, no details found for this vehicle.</p>'
+    }
+    return details
+  }
 
-/* ****************************************
- * Middleware For Handling Errors
- **************************************** */
-Util.handleErrors = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications()
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>'
+    classificationList += "<option value=''>Choose a Classification</option>"
+    data.rows.forEach((row) => {
+      classificationList += '<option value="' + row.classification_id + '"'
+      if (
+        classification_id != null &&
+        row.classification_id == classification_id
+      ) {
+        classificationList += " selected "
+      }
+      classificationList += ">" + row.classification_name + "</option>"
+    })
+    classificationList += "</select>"
+    return classificationList
+}  
 
-/* ****************************************
+/* **
  * Middleware to check token validity
- **************************************** */
+ * **/
 Util.checkJWTToken = (req, res, next) => {
-  const token = req.cookies.jwt;
-  console.log("JWT Cookie:", token);
-
-  if (!token) {
-    res.locals.loggedIn = false;
-    console.log("No token found. Redirecting to login.");
-    return next();
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    res.locals.accountData = decoded;
-    res.locals.loggedIn = true;
-    console.log("Logged In Status:", res.locals.loggedIn);
-    res.locals.firstName = decoded.first_name;
-    res.locals.accountType = decoded.account_type;
-    console.log("Decoded Token Payload:", decoded);
-    return next();
-  } catch (err) {
-    console.error("JWT Verification Error:", err.message);
-    req.flash("notice", "Session expired or invalid. Please log in again.");
-    res.clearCookie("jwt");
-    return res.redirect("/account/login");
-  }
-};
-
-/* ****************************************
- * Middleware to check login
- **************************************** */
-Util.checkLogin = (req, res, next) => {
-  if (res.locals.loggedIn) {
-    next();
+  if (req.cookies.jwt) {
+    jwt.verify(
+      req.cookies.jwt,
+      process.env.ACCESS_TOKEN_SECRET,
+      function (err, accountData) {
+        if (err) {
+          req.flash("Please log in")
+          res.clearCookie("jwt")
+          res.locals.loggedin = false
+          return res.redirect("/account/login")
+        }
+        res.locals.accountData = accountData
+        res.locals.loggedin = true
+        next()
+      }
+    )
   } else {
-    req.flash("notice", "Please log in.");
-    return res.redirect("/account/login");
+    res.locals.loggedin = false
+    next()
   }
-};
+}
 
-module.exports = Util;
+/* **
+ * Check Login
+ * **/
+Util.checkLogin = (req, res, next) => {
+  if (res.locals.loggedin) {
+    next()
+  } else {
+    req.flash("validation-notice", "Please log in.")
+    return res.redirect("/account/login")
+  }
+}
+
+/* **
+ * Check Account type
+ * **/
+Util.checkAccountType = (req, res, next) => {
+  const accountData = res.locals.accountData
+  // if no user data, login is required to access this view
+  if (!accountData) {
+    req.flash("validation-notice", "Please Log in to continue.")
+    return res.redirect("/account/login")
+  }
+  
+  // check the account type
+  if (accountData.account_type === "Employee" || accountData.account_type === "Admin") {
+    return next()
+  } else{
+    req.flash("validation-notice", "You do not have the permission to access this page.")
+    return res.redirect("/account/login")
+  }
+}
+
+/* **
+ * Middleware For Handling Errors
+ * Wrap other function in this for
+ * General Error Handling
+ * **/
+Util.handleErrors  = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+  
+module.exports = Util
